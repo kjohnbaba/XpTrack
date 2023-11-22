9b0de342789a7f925c77bbb67b68d3727c6c265c
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-11-22 16:12:31 -0500

    Update UserData.java

--- app/src/main/java/com/example/xptrack/UserData.java
+++ app/src/main/java/com/example/xptrack/UserData.java
@@ -4,7 +4,7 @@ public class UserData {
 
     private int totalXP;
     private int strength;
-    
+
     public UserData(int totalXP, int strength) {
         this.totalXP = totalXP;
         this.strength = strength;
@@ -13,10 +13,19 @@ public class UserData {
     public UserData() {
     }
 
+    public int getTotalXP() {
+        return totalXP;
+    }
 
+    public void setTotalXP(int totalXP) {
+        this.totalXP = totalXP;
+    }
 
+    public int getStrength() {
+        return strength;
+    }
 
-
-
-
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 }
