7c48c034e7cabf2f4edc8a76f9f384961d55799f
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2023-11-22 16:10:34 -0500

    Update UserData.java

--- app/src/main/java/com/example/xptrack/UserData.java
+++ app/src/main/java/com/example/xptrack/UserData.java
@@ -1,6 +1,15 @@
 package com.example.xptrack;
 
 public class UserData {
+
     private int totalXP;
     private int strength;
+
+    // constructors to add
+
+
+    public UserData(int totalXP, int strength) {
+        this.totalXP = totalXP;
+        this.strength = strength;
+    }
 }
